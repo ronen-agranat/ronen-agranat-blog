9fe79f75223cbca5fd0c3f67139bda05f8b0848c
Ronen Agranat <ronen.agranat@booking.com>
2019-08-25 19:10:19 +0200

    Remove Twitter feed from front page
    - Re-add the 'fork me' buttons and copy
    
    - Update commands so that it's not necessary to be in blog dir

--- commands.sh
+++ commands.sh
@@ -2,6 +2,7 @@
 # Source this in your .zshrc file or similar
 
 # Build and deploy
-alias blogsync="JEKYLL_ENV=production bundle exec jekyll b && cd _site && aws s3 sync . s3://ronenagranat.com/ && cd -"
-alias bloglocal="bundle exec jekyll serve"
+export BLOG_DIR="/Users/ragranat/personal/ronenagranat"
+alias blogsync="cd $BLOG_DIR && JEKYLL_ENV=production bundle exec jekyll b && cd _site && aws s3 sync . s3://ronenagranat.com/ && cd -"
+alias bloglocal="cd $BLOG_DIR && bundle exec jekyll serve"
 
